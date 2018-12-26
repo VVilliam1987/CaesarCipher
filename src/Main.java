54107e660d86d4a61e90ace5d47f9ad1ed3ad9ae
Mykola <46055946+FriedrichMykola@users.noreply.github.com>
2018-12-26 22:36:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,24 @@ public class Main {
     public static void main(String[] args) {
 
         EnterText text = new EnterText();
+        /*
+        
+        class MyClass
+        {
+        
+            public function(String ss)
+            {
+                // some code
+            }
+        }
+        
+        Main()
+        {
+            MyClass my = new MyClass();
+            my.function(text.getSomeValue);
+        }
+        
+        */
         text.start();
     }
 }
